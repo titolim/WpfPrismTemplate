539b444e0d51aa59f2d8e65369df5e1619d00c71
Tito Lim <titolim@users.noreply.github.com>
2019-05-24 16:18:36 -0400

    First check-in

---
+++ WpfPrismTemplate/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfPrismTemplate
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : PrismApplication
+  {
+    protected override Window CreateShell()
+    {
+      return Container.Resolve<MainWindow>();
+    }
+
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+    }
+
+    protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+    {
+      moduleCatalog.AddModule<ModuleA.ModuleAModule>();
+    }
+  }
+}
